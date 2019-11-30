171861ecfb89bd082bc83e9344ae4fe8b0bf72ad
el-fudo <fumitake.kawasaki@el-soul.com>
2019-12-01 00:44:02 +0900

    add review method

--- lib/hotel_price.rb
+++ lib/hotel_price.rb
@@ -6,11 +6,21 @@ module HotelPrice
   def self.rakuten_travel
     # firefox_capabilities = Selenium::WebDriver::Remote::Capabilities.firefox
     # @driver = Selenium::WebDriver.for(:remote, url: "http://hub:4444/wd/hub", desired_capabilities: firefox_capabilities)
-    driver = Selenium::WebDriver.for :firefox
-    driver.get('https://travel.rakuten.co.jp/')
+    options = Selenium::WebDriver::Firefox::Options.new
+    options.add_argument("-headless")
+    driver = Selenium::WebDriver.for :firefox, options: options
+    rakuten_travel_hotel_id = 7770
+    driver.get("https://travel.rakuten.co.jp/HOTEL/#{rakuten_travel_hotel_id.to_s}/review.html")
     sleep 2
-    title = driver.find_element(:tag_name, "p").text
+    comment_area = driver.find_elements(:class_name, "commentReputationBoth")
+    data = comment_area.map do |f|
+      {
+        date: f.find_element(class_name: "time").text,
+        rakuten_hotel_id: 7770,
+        comment: f.find_element(class_name: "commentSentence").text
+      }
+    end
     driver.quit
-    title
+    data
   end
 end
--- lib/rakuten_travel_review.rb
+++ lib/rakuten_travel_review.rb
@@ -6,8 +6,11 @@ module RakutenTravelReview
   def self.rakuten_travel
     # firefox_capabilities = Selenium::WebDriver::Remote::Capabilities.firefox
     # @driver = Selenium::WebDriver.for(:remote, url: "http://hub:4444/wd/hub", desired_capabilities: firefox_capabilities)
-    driver = Selenium::WebDriver.for :firefox
-    driver.get('https://travel.rakuten.co.jp/')
+    options = Selenium::WebDriver::Firefox::Options.new
+    options.add_argument("-headless")
+    driver = Selenium::WebDriver.for :firefox, options: options
+    base_url = "https://travel.rakuten.co.jp/HOTEL/7770/review.html"
+    driver.get base_url
     sleep 2
     title = driver.find_element(:tag_name, "p").text
     driver.quit
