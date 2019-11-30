ce79ed0866d0f87e117fb49f9ad97fe3c118bd0b
el-fudo <fumitake.kawasaki@el-soul.com>
2019-11-30 17:34:26 +0900

    add rakuten travel

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 
 # Specify your gem's dependencies in hotel_price.gemspec
 gemspec
+gem "selenium-webdriver", "~> 3.141"
--- lib/hotel_price.rb
+++ lib/hotel_price.rb
@@ -1,8 +1,16 @@
 require "hotel_price/version"
+require "selenium-webdriver"
 
 module HotelPrice
   class Error < StandardError; end
-  def self.greet
-    "Hello"
+  def self.rakuten_travel
+    # firefox_capabilities = Selenium::WebDriver::Remote::Capabilities.firefox
+    # @driver = Selenium::WebDriver.for(:remote, url: "http://hub:4444/wd/hub", desired_capabilities: firefox_capabilities)
+    driver = Selenium::WebDriver.for :firefox
+    driver.get('https://travel.rakuten.co.jp/')
+    sleep 2
+    title = driver.find_element(:tag_name, "p").text
+    driver.quit
+    title
   end
 end
