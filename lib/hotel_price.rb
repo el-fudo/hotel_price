fcc89c11c282db21b7ee94c754f08902c37b22af
el-fudo <fumitake.kawasaki@el-soul.com>
2020-01-15 22:35:45 +0900

    add review test

--- lib/hotel_price.rb
+++ lib/hotel_price.rb
@@ -43,6 +43,26 @@ module HotelPrice
   end
 
   module RakutenTravel
+    def self.review rakuten_hotel_id
+      # firefox_capabilities = Selenium::WebDriver::Remote::Capabilities.firefox
+      # @driver = Selenium::WebDriver.for(:remote, url: "http://hub:4444/wd/hub", desired_capabilities: firefox_capabilities)
+      options = Selenium::WebDriver::Firefox::Options.new
+      options.add_argument("-headless")
+      driver = Selenium::WebDriver.for :firefox, options: options
+      driver.get("https://travel.rakuten.co.jp/HOTEL/#{rakuten_hotel_id.to_s}/review.html")
+      sleep 2
+      comment_area = driver.find_elements(:class_name, "commentBox")
+      data = comment_area.map do |f|
+        {
+          date: f.find_element(class_name: "time").text,
+          rakuten_hotel_id: rakuten_hotel_id,
+          comment: f.find_element(class_name: "commentSentence").text
+        }
+      end
+      driver.quit
+      data
+    end
+
     class RakutenConsole
       def initialize params
         @config = {
--- spec/hotel_price_spec.rb
+++ spec/hotel_price_spec.rb
@@ -4,6 +4,14 @@ RSpec.describe HotelPrice do
     expect(HotelPrice::VERSION).not_to be nil
   end
 
+  describe "Rakuten Travel Review" do
+    ## Scraper Test
+    # it "Get All Reviews" do
+    #   a1 = HotelPrice::RakutenTravel.review "128552"
+    #   expect(a1[0][:rakuten_hotel_id]).to eq "128552"
+    # end
+  end
+
   describe "Rakuten Travel API" do
     before(:each) do
       @a1 = HotelPrice::RakutenTravel::RakutenAPI.new(
@@ -12,7 +20,6 @@ RSpec.describe HotelPrice do
     end
 
     it "Test" do
-      puts @a1.test
       expect(@a1.test[:rakuten_hotel_id]).to eq "128552"
     end
 
@@ -21,13 +28,14 @@ RSpec.describe HotelPrice do
     end
   end
 
+
   describe "Rakuten Travel Console" do
     before(:each) do
       @a1 = HotelPrice::RakutenTravel::RakutenConsole.new(
         login_id: "login-id",
         login_pw: "login-pw",
-        chain: true,
-        rakuten_hotel_id: "128552"
+        rakuten_hotel_id: "128552",
+        chain: false,
       )
     end
 
