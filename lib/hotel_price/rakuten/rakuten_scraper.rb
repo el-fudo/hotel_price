0cfdf37df8e7ed56000cbbb37f1bdba572cd1a18
el-fudo <fumitake.kawasaki@el-soul.com>
2020-01-30 03:08:23 +0900

    add rakuten scraper

---
+++ lib/hotel_price/rakuten/rakuten_scraper.rb
@@ -0,0 +1,19 @@
+module HotelPrice::Rakuten
+  class RakutenScraper
+    def self.review rakuten_hotel_id
+      driver = self.get_selenium_driver
+      driver.get("https://travel.rakuten.co.jp/HOTEL/#{rakuten_hotel_id.to_s}/review.html")
+      sleep 2
+      comment_area = driver.find_elements(:class_name, "commentBox")
+      data = comment_area.map do |f|
+        {
+            date: f.find_element(class_name: "time").text,
+            rakuten_hotel_id: rakuten_hotel_id,
+            comment: f.find_element(class_name: "commentSentence").text
+        }
+      end
+      driver.quit
+      data
+    end
+  end
+end
--- lib/hotel_price/version.rb
+++ lib/hotel_price/version.rb
@@ -1,3 +1,3 @@
 module HotelPrice
-  VERSION = "0.2.3"
+  VERSION = "0.2.4"
 end
