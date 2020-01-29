c95bed3bfe9fded7105a2a3747fb3ce0bb1b3ae2
el-fudo <fumitake.kawasaki@el-soul.com>
2020-01-29 23:21:12 +0900

    add jalan get price

--- lib/hotel_price/jalan/jalan_api.rb
+++ lib/hotel_price/jalan/jalan_api.rb
@@ -7,5 +7,33 @@ module HotelPrice::Jalan
     def test
       @api_key
     end
+
+    def get_price
+      if @doc.css("NumberOfResults").text == "0"
+        {
+          date: DateTime.now.strftime("%Y-%m-%d"),
+          branch_id: @user_info[:branch_id],
+          stay_date: @user_info[:checkindate],
+          adult_num: @user_info[:adult_num],
+          jalan_hotel_id: @user_info[:jalan_hotel_id],
+          plan_num: 0,
+          min_price: 0
+        }
+      else
+        {
+          date: DateTime.now.strftime("%Y-%m-%d"),
+          branch_id: @user_info[:branch_id],
+          channel: "jalan",
+          stay_date: @user_info[:checkindate],
+          adult_num: @user_info[:adult_num],
+          jalan_hotel_id: @user_info[:jalan_hotel_id],
+          hotel_name: @doc.css("Plan").css("Hotel HotelName").text,
+          room_name: @doc.css("Plan").css("RoomName").text,
+          plan_name: @doc.css("Plan").css("PlanName").text,
+          plan_num: @doc.css("NumberOfResults").text,
+          min_price: @doc.css("Plan").css("Stay Rate").text
+        }
+      end
+    end
   end
 end
