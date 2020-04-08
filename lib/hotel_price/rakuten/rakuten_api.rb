644300cb42615d9f8967d3a3f303181e08a1a219
FUMI-POPPIN <fumitake.kawasaki@el-soul.com>
2020-04-07 17:42:18 -0700

    fix rakuten api hotel info

--- lib/hotel_price/rakuten/rakuten_api.rb
+++ lib/hotel_price/rakuten/rakuten_api.rb
@@ -126,39 +126,39 @@ module HotelPrice::Rakuten
       end
       result["hotels"][0].each do |_key, field|
         field[1].each do |_, value|
-          @data_hash["rakuten_service_review"] = value["serviceAverage"].to_f
-          @data_hash["rakuten_location_review"] = value["locationAverage"].to_f
-          @data_hash["rakuten_room_review"] = value["roomAverage"].to_f
-          @data_hash["rakuten_equipment_review"] = value["equipmentAverage"].to_f
-          @data_hash["rakuten_bath_review"] = value["bathAverage"].to_f
-          @data_hash["rakuten_meal_review"] = value["mealAverage"].to_f
+          @data_hash[:rakuten_service_review] = value["serviceAverage"].to_f
+          @data_hash[:rakuten_location_review] = value["locationAverage"].to_f
+          @data_hash[:rakuten_room_review] = value["roomAverage"].to_f
+          @data_hash[:rakuten_equipment_review] = value["equipmentAverage"].to_f
+          @data_hash[:rakuten_bath_review] = value["bathAverage"].to_f
+          @data_hash[:rakuten_meal_review] = value["mealAverage"].to_f
         end
       end
       result["hotels"][0].each do |_key, field|
         field[2].each do |_, value|
-          @data_hash["middle_class_code"] = value["middleClassCode"].to_s
-          @data_hash["small_class_code"] = value["smallClassCode"].to_s
-          @data_hash["area_name"] = value["areaName"].to_s
-          @data_hash["hotel_class_code"] = value["hotelClassCode"].to_s
-          @data_hash["checkin_time"] = value["checkinTime"].to_s
-          @data_hash["checkout_time"] = value["checkoutTime"].to_s
-          @data_hash["last_checkin_time"] = value["lastCheckinTime"].to_s
+          @data_hash[:middle_class_code] = value["middleClassCode"].to_s
+          @data_hash[:small_class_code] = value["smallClassCode"].to_s
+          @data_hash[:area_name] = value["areaName"].to_s
+          @data_hash[:hotel_class_code] = value["hotelClassCode"].to_s
+          @data_hash[:checkin_time] = value["checkinTime"].to_s
+          @data_hash[:checkout_time] = value["checkoutTime"].to_s
+          @data_hash[:last_checkin_time] = value["lastCheckinTime"].to_s
         end
       end
       result["hotels"][0].each do |_key, field|
         field[3].each do |_, value|
-          @data_hash["total_room_num"] = value["hotelRoomNum"].to_s
+          @data_hash[:total_room_num] = value["hotelRoomNum"].to_s
           room_facilities = []
           value["roomFacilities"].each_with_index do |f, i|
             room_facilities[i] = f["item"]
           end
-          @data_hash["room_facilities"] = room_facilities
+          @data_hash[:room_facilities] = room_facilities
         end
       end
       result["hotels"][0].each do |_key, field|
         field[4].each do |_, value|
-          @data_hash["hotel_policy_note"] = value["note"].to_s
-          @data_hash["cancel_policy"] = value["cancelPolicy"].to_s
+          @data_hash[:hotel_policy_note] = value["note"].to_s
+          @data_hash[:cancel_policy] = value["cancelPolicy"].to_s
         end
       end
       @data_hash
--- lib/hotel_price/version.rb
+++ lib/hotel_price/version.rb
@@ -1,3 +1,3 @@
 module HotelPrice
-  VERSION = "0.4.0"
+  VERSION = "0.4.1"
 end
